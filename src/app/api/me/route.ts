375ff214c5913540190239df0e65f319acda751e
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2024-10-27 17:20:27 -0600

    feat(api): add routes for sign up and validation session token

---
+++ src/app/api/me/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import { headers } from "next/headers";
+import { JwtPayload } from 'jsonwebtoken';
+import HTTPStatus from 'http-status-codes'
+import * as jwt from '../../utils/jwt'
+
+export async function GET() {
+  try {
+    const headersInstance = headers()
+    const authHeader = (await headersInstance).get('authorization')
+    const token = authHeader?.split(' ')[1] || ''
+    const decoded = jwt.verify(token)
+    if (!decoded) {
+      return NextResponse.json({
+        message: 'Token expired'
+      }, {
+        status: HTTPStatus.NOT_ACCEPTABLE
+      })
+    } else if (((decoded as unknown as JwtPayload).exp as number) < Math.floor(Date.now() / 1000)) {
+      return NextResponse.json({
+        message: 'Token expired'
+      }, {
+        status: HTTPStatus.NOT_ACCEPTABLE
+      })
+    } else {
+      return NextResponse.json({
+        message: 'User accepted'
+      }, {
+        status: HTTPStatus.OK
+      })
+    }
+  } catch (e) {
+    console.error(e)
+    return NextResponse.json({
+        message: 'Unauthorized'
+      }, {
+        status: HTTPStatus.UNAUTHORIZED
+      })
+  }
+}
---
+++ src/app/api/signup/route.ts
@@ -0,0 +1,45 @@
+import HTTPStatus from 'http-status-codes';
+import client from '../../../prisma/client'
+import * as pwd from '../../utils/password'
+import * as jwt from '../../utils/jwt';
+import { NextRequest, NextResponse } from 'next/server';
+
+
+type Body = {
+  username: string
+  password: string
+}
+
+export async function POST(
+  req: NextRequest,
+) {
+  const body: Body = await req.json()
+  
+  const {
+    username,
+    password
+  } = body
+  try {
+    const hash = pwd.encrypt(password)
+    const result = await client.user.create({
+      data: {
+        username: username,
+        password: hash
+      }
+    })
+    const token = jwt.sign(result.id)
+    return NextResponse.json({
+      message: 'User created',
+      data: result.id,
+      jwt: token
+    }, {
+      status: HTTPStatus.CREATED
+    })
+  } catch (e) {
+    return NextResponse.json({
+      message: new String(e),
+    }, {
+      status: HTTPStatus.BAD_REQUEST
+    })
+  }
+}
