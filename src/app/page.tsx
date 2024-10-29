36fe700bc7077b26a35e56931e3d0477aac71a9b
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2024-10-28 22:51:53 -0600

    feat(index): add flights at index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
 'use client'
 
-import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { home as locationData } from "./utils/amadeus";
+import FlightList from "@/components/List/Flights";
 
 export default function Home() {
+  const now = new Date();
 
+  if (now.getHours() >= 18) {
+    // Move dateObj to the next day
+    now.setDate(now.getDate() + 1);
+  }
+  // Format the date as "YYYY-MM-DD" in local time
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed, so we add 1
+  const day = String(now.getDate()).padStart(2, '0');
+
+  // Combine to get the formatted date
+  const formattedDate = `${year}-${month}-${day}`;
+  const [date, setDate] = useState(formattedDate)
+  const [flights, setFlights] = useState([])
   useEffect(() => {
     const geolocation = (() => {
       function get(): Promise<{ pos: GeolocationPosition } | { err: string }> {
@@ -49,32 +63,41 @@ export default function Home() {
 
           console.log(latitude, longitude);
           
-          // Use latitude and longitude here as needed
           const params = new URLSearchParams({
             latitude: latitude.toString(),
             longitude: longitude.toString()
           });
 
-          console.log("URL Params:", params.toString());
           const airports = await locationData.nearAirports(params.toString())
 
-          console.log(airports);
           const rootFlights = []
-          for (let airport of airports) {
+          for (const airport of airports) {
             const params = new URLSearchParams({
               departureAirportCode: airport.iataCode,
               max: '2'
             })
             const destinations = await locationData.topDestinations(params.toString())
-            for (let destination of destinations) {
+            for (const destination of destinations) {
               rootFlights.push({
                 origin: airport.iataCode,
-                departure: destination.iataCode
+                destination: destination.iataCode
               })
             }
           }
           console.log(rootFlights)
-
+          const amadeusFlights = []
+          for (const flight of rootFlights) {
+            const params = new URLSearchParams({
+              originLocationCode: flight.origin,
+              destinationLocationCode: flight.destination,
+              departureDate: date,
+              adults: '1',
+            })
+            const resultFlight = await locationData.getFlights(params.toString())
+            setFlights((prev) => [...prev, ...resultFlight])
+          }
+          console.log(amadeusFlights)
+          
         }
         return false
       } catch (err) {
@@ -88,60 +111,28 @@ export default function Home() {
     fetchLocation()
   }, [])
 
+  const onUpdateDepartureDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDate(e.target.value)
+  }
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          {/* <p>{`Your location is ${pos?.coords.altitude} ${pos?.coords.latitude}`}</p> */}
+          <input
+            type='date'
+            onChange={onUpdateDepartureDate}
+            value={date} />
+          {date}
+
+          <div>
+            <FlightList flights={flights} page={1} limit={10} />
+          </div>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        
       </footer>
     </div>
   );
