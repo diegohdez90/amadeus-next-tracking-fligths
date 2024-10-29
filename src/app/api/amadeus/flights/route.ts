365c3f3696992f1136c90ef52152c598a9ed08f2
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2024-10-28 22:45:41 -0600

    fix(api): update req query params to search flights

--- src/app/api/amadeus/flights/route.ts
+++ src/app/api/amadeus/flights/route.ts
@@ -1,14 +1,25 @@
 import amadeus from "@/app/utils/amadeus";
 import HTTPStatus from 'http-status-codes';
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  const res = await amadeus.shopping.flightOffersSearch.get()
+export async function GET(req: NextRequest) {
+    const originLocationCode = req.nextUrl.searchParams.get('originLocationCode') as string
+    const destinationLocationCode = req.nextUrl.searchParams.get('destinationLocationCode') as string
+    const departureDate = req.nextUrl.searchParams.get('departureDate') as string
+    const adults = req.nextUrl.searchParams.get('adults') as unknown as number
+
+  const res = await amadeus.shopping.flightOffersSearch.get({
+    originLocationCode,
+    destinationLocationCode,
+    departureDate,
+    adults
+  })
   
   if (res.statusCode == HTTPStatus.OK) {
     return NextResponse.json({
       message: 'offers',
-      data: res.data
+      data: res.data,
+      dictionaries: res.data?.dictionaries
     }, {
       status: HTTPStatus.OK
     });  
